51be1a368c6186029c053fd6e83fac2607f3ae2b
befabry <fabrybenjamin5@gmail.com>
2020-08-07 17:36:40 +0200

    loginRoute + override the body property

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+
+import { router } from "./Routes/loginRoutes";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send(`
-      <div>
-        <h1>Hi</h1>
-      </div>
-    `);
-});
+//The body property exist only because we have a middleware body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
